d12d2fa2a5723d32bde2efc9f16767e0502e796d
og.ubeda <og.ubeda@gmail.com>
2021-01-14 21:06:07 +0100

    Cambios

--- activitat1/main.js
+++ activitat1/main.js
@@ -1,10 +1,9 @@
-function multiply(a, b = 1) {
-    return a * b;
-  }
-  
-  console.log(multiply(5, 2));
-  // expected output: 10
-  
-  console.log(multiply(5));
-  // expected output: 5
-  
+function multiply (a, b = 1) {
+    return a * b
+}
+
+console.log(multiply(5, 2))
+// expected output: 10
+
+console.log(multiply(5))
+// expected output: 5
