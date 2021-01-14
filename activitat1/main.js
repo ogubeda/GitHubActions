d4ecf42bfa7b090668612ebb1a868518833b2155
og.ubeda <og.ubeda@gmail.com>
2021-01-14 21:18:37 +0100

    Cambios

--- activitat1/main.js
+++ activitat1/main.js
@@ -1,8 +1,8 @@
 function multiply (a, b = 1) {
-  return a * b
+  return a * b;
 }
 
-console.log(multiply(5, 2))
+console.log(multiply(5, 2));
 // expected output: 10
 
-console.log(multiply(5))
+console.log(multiply(5));
