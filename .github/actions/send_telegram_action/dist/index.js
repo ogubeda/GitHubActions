9816ba5365113851203fab2e5a593b818f233665
ogubeda <og.ubeda@gmail.com>
2021-01-28 21:23:12 +0100

    Test

--- .github/actions/send_telegram_action/dist/index.js
+++ .github/actions/send_telegram_action/dist/index.js
@@ -201,9 +201,9 @@ const core = __nccwpck_require__(6046);
 const TelegramBot = __nccwpck_require__(2570);
 const github = __nccwpck_require__(9971) 
 
-const token = core.getInput("token_father");
+const token = core.getInput("tokenFather");
 const bot = new TelegramBot(token, {polling: false});
-var chat_Id=core.getInput("chatId");
+var chat_Id=core.getInput("chatID");
 const name = core.getInput("name");
 const commit = github.context.payload;
 console.log(commit);
@@ -211,7 +211,7 @@ console.log(commit);
 
 
 try {
-    bot.sendMessage(chat_Id,`Workflow executed. Greetings ,${name}:\n\nCommited By:${commit.head_commit.author.name}\Avatar: ${commit.sender.avatar_url}\Url: ${commit.head_commit.url}\Message: ${commit.head_commit.message}`);
+    bot.sendMessage(chat_ID,`Workflow executed. Greetings ,${name}:\n\nCommited By:${commit.head_commit.author.name}\Avatar: ${commit.sender.avatar_url}\Url: ${commit.head_commit.url}\Message: ${commit.head_commit.message}`);
   
   } catch (error) {
     core.setFailed(error.message);
--- .github/actions/send_telegram_action/index.js
+++ .github/actions/send_telegram_action/index.js
@@ -2,9 +2,9 @@ const core = require("@actions/core");
 const TelegramBot = require('node-telegram-bot-api');
 const github = require("@actions/github") 
 
-const token = core.getInput("token_father");
+const token = core.getInput("tokenFather");
 const bot = new TelegramBot(token, {polling: false});
-var chat_Id=core.getInput("chatId");
+var chat_Id=core.getInput("chatID");
 const name = core.getInput("name");
 const commit = github.context.payload;
 console.log(commit);
@@ -12,7 +12,7 @@ console.log(commit);
 
 
 try {
-    bot.sendMessage(chat_Id,`Workflow executed. Greetings ,${name}:\n\nCommited By:${commit.head_commit.author.name}\Avatar: ${commit.sender.avatar_url}\Url: ${commit.head_commit.url}\Message: ${commit.head_commit.message}`);
+    bot.sendMessage(chat_ID,`Workflow executed. Greetings ,${name}:\n\nCommited By:${commit.head_commit.author.name}\Avatar: ${commit.sender.avatar_url}\Url: ${commit.head_commit.url}\Message: ${commit.head_commit.message}`);
   
   } catch (error) {
     core.setFailed(error.message);
