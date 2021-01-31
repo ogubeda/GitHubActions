4ec35d906517f458558d392b61c4e4d0e08e2220
ogubeda <og.ubeda@gmail.com>
2021-01-31 15:46:46 +0100

    Test

--- .github/actions/send_telegram_action/index.js
+++ .github/actions/send_telegram_action/index.js
@@ -1,6 +1,8 @@
 const core = require("@actions/core");
 const TelegramBot = require('node-telegram-bot-api');
+const github = require("@actions/github") 
 
+const commit = github.context.payload;
 const token = core.getInput("tokenFather");
 const bot = new TelegramBot(token, {polling: false});
 var chatID = core.getInput("chatID");
@@ -8,8 +10,9 @@ const name = core.getInput("name");
 
 
 
+
 try {
-    bot.sendMessage(chatID,`Workflow executed. Greetings ,${name}:\n\n`);
+    bot.sendMessage(chatID,`Workflow executed. Commit: ${commit.head_commit.author.name}\nAvatar: ${commit.sender.avatar_url}\nURL: ${commit.head_commit.url}. Greetings, ${name}:\n\n`);
   
   } catch (error) {
     core.setFailed(error.message);
